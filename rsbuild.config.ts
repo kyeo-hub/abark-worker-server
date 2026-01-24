497cdbad084a5851e2a5a10094df7f2bb6a3a57e
泷涯 <i@sylingd.com>
2026-01-24 17:49:08 +0800

    feat: use eo node functions to proxy APNs (#5)

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -1,31 +1,19 @@
 import path from 'node:path';
-import { defineConfig } from '@rsbuild/core';
+import { fileURLToPath } from 'node:url';
+import { defineConfig, type RsbuildConfig } from '@rsbuild/core';
 import fs from 'fs-extra';
+import { nanoid } from 'nanoid';
 
-const defines: any = {
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const defines: Record<string, string> = {
   'process.env.ENTRY': JSON.stringify(process.env.ENTRY),
 };
 
-if (process.env.ENTRY === 'esa') {
-  [
-    'URL_PREFIX',
-    'MAX_BATCH_PUSH_COUNT',
-    'DB_NAME',
-    'ALLOW_NEW_DEVICE',
-    'ALLOW_QUERY_NUMS',
-    'BASIC_AUTH',
-    'APNS_URL',
-  ].forEach((key) => {
-    defines[`process.env.${key}`] = process.env[key]
-      ? JSON.stringify(process.env[key])
-      : 'undefined';
-  });
-}
-
-// Docs: https://rsbuild.rs/config/
-export default defineConfig({
+const config: RsbuildConfig = {
   output: {
-    target: process.env.ENTRY === 'node' ? 'node' : 'web',
+    target: 'web',
     module: true,
     distPath: {
       root: 'dist',
@@ -56,7 +44,7 @@ export default defineConfig({
   },
   tools: {
     rspack: {
-      target: process.env.ENTRY === 'node' ? 'node' : 'es2020',
+      target: 'es2020',
       output: {
         asyncChunks: false,
         library: {
@@ -68,29 +56,113 @@ export default defineConfig({
       },
     },
   },
-  plugins: [
+};
+
+if (process.env.ENTRY === 'node') {
+  config.output!.target = 'node';
+}
+
+if (process.env.ENTRY === 'esa') {
+  [
+    'URL_PREFIX',
+    'MAX_BATCH_PUSH_COUNT',
+    'DB_NAME',
+    'ALLOW_NEW_DEVICE',
+    'ALLOW_QUERY_NUMS',
+    'BASIC_AUTH',
+    'APNS_URL',
+  ].forEach((key) => {
+    defines[`process.env.${key}`] = process.env[key]
+      ? JSON.stringify(process.env[key])
+      : 'undefined';
+  });
+}
+
+if (process.env.ENTRY === 'edgeone') {
+  let proxyToken = process.env.PROXY_TOKEN;
+  if (!proxyToken) {
+    proxyToken = nanoid();
+    console.log(`Generate PROXY_TOKEN: ${proxyToken}`);
+  } else {
+    console.log(`Use PROXY_TOKEN: ${proxyToken}`);
+  }
+  defines['process.env.PROXY_TOKEN'] = JSON.stringify(proxyToken);
+
+  if (!process.env.URL_PREFIX || process.env.URL_PREFIX === '/') {
+    throw new Error('Please set URL_PREFIX');
+  }
+
+  config.environments = {
+    'node-proxy': {
+      output: {
+        target: 'node',
+      },
+      tools: {
+        rspack: {
+          target: 'node',
+        },
+      },
+      source: {
+        entry: {
+          'apns-proxy': {
+            import: `./src/entry/edgeone-apns-proxy.ts`,
+            html: false,
+          },
+        },
+      },
+    },
+  };
+
+  config.plugins = [
     {
       name: 'after',
       setup(api) {
         api.onAfterBuild(async () => {
           if (process.env.ENTRY === 'edgeone') {
+            if (!process.env.URL_PREFIX || process.env.URL_PREFIX === '/') {
+              throw new Error('Please set URL_PREFIX');
+            }
             const cwd = process.cwd();
             const dist = path.join(__dirname, 'dist');
-            let functions = path.join(cwd, 'edge-functions');
-            if (process.env.URL_PREFIX) {
-              console.log(`Detected URL_PREFIX: ${process.env.URL_PREFIX}`);
-              functions = path.join(functions, process.env.URL_PREFIX);
-            }
+            const functions = path.join(
+              cwd,
+              'edge-functions',
+              process.env.URL_PREFIX,
+            );
+            const nodeFunctions = path.join(
+              cwd,
+              'node-functions',
+              'bark-node',
+              process.env.URL_PREFIX,
+            );
             await fs.ensureDir(functions);
+            await fs.ensureDir(nodeFunctions);
             const target = path.join(functions, '[[default]].js');
             if (await fs.pathExists(target)) {
               await fs.remove(target);
             }
             console.log(`Move handler.js to ${target}`);
             await fs.move(path.join(dist, 'handler.js'), target);
+
+            const nodeTarget = path.join(nodeFunctions, 'apns-proxy.js');
+            console.log(`Move apns-proxy.js to ${nodeTarget}`);
+            const nodeTargetContent = await fs.readFile(
+              path.join(dist, 'apns-proxy.js'),
+              'utf-8',
+            );
+            await fs.writeFile(
+              nodeTarget,
+              nodeTargetContent.replace(
+                /export[\s]*{(.*?) as handleRequest[\s]*}/,
+                'export const onRequest=$1',
+              ),
+            );
           }
         });
       },
     },
-  ],
-});
+  ];
+}
+
+// Docs: https://rsbuild.rs/config/
+export default defineConfig(config);
--- src/entry/edgeone.ts
+++ src/entry/edgeone.ts
@@ -1,7 +1,14 @@
 import type { Env, Hono } from 'hono';
+import { nanoid } from 'nanoid/non-secure';
 import { KVAdapter } from '../core/db/kv-adapter';
 import { createHono } from '../core/hono';
-import type { BasicEnv } from '../core/type';
+import type {
+  APNsProxyItem,
+  APNsProxyResponse,
+  APNsResponse,
+  BasicEnv,
+  Options,
+} from '../core/type';
 
 interface EOEventContext {
   params: any;
@@ -15,6 +22,86 @@ interface EOHonoEnv extends Env {
 
 let hono: Hono<EOHonoEnv>;
 
+interface QueueItem extends APNsProxyItem {
+  resolve: (value: APNsResponse) => void;
+  reject: (reason?: Error) => void;
+}
+
+interface IAPNsProxy {
+  host: string;
+  prefix: string;
+  token: string;
+  queue: QueueItem[];
+  timer: ReturnType<typeof setTimeout> | undefined;
+  execute: () => Promise<void>;
+  request: NonNullable<Options['requestAPNs']>;
+}
+
+const APNsProxy: IAPNsProxy = {
+  host: '',
+  prefix: '',
+  token: process.env.PROXY_TOKEN as string,
+  queue: [],
+  timer: undefined,
+  execute: async function (this: IAPNsProxy) {
+    this.timer = undefined;
+    const cloneQueue = [...this.queue];
+    this.queue = [];
+    try {
+      const f = await fetch(
+        `https://${this.host}/bark-node${this.prefix}/apns-proxy`,
+        {
+          method: 'POST',
+          headers: {
+            'x-token': this.token,
+            'content-type': 'application/json',
+          },
+          body: JSON.stringify(cloneQueue),
+        },
+      );
+      const resp = await f.json();
+      if (!resp.data) {
+        throw new Error('Execute queue failed');
+      }
+      resp.data.forEach((item: APNsProxyResponse) => {
+        cloneQueue.find((x) => x.id === item.id)?.resolve(item);
+      });
+    } catch (e) {
+      cloneQueue.forEach((x) => x.reject(e as Error));
+    }
+  },
+  request: function (this: IAPNsProxy, deviceToken, headers, aps, ctx) {
+    if (!ctx) {
+      throw new Error('ctx is not defined');
+    }
+    const env: BasicEnv = ctx.env;
+    if (env.PROXY_TOKEN) {
+      this.token = env.PROXY_TOKEN;
+    }
+    this.host = String(ctx.req.header('host'));
+    this.prefix = String(env.URL_PREFIX);
+
+    return new Promise((resolve, reject) => {
+      let id = nanoid();
+      while (this.queue.some((x) => x.id === id)) {
+        id = nanoid();
+      }
+      this.queue.push({
+        id,
+        deviceToken,
+        headers,
+        aps,
+        resolve,
+        reject,
+      });
+      if (typeof this.timer !== 'undefined') {
+        return;
+      }
+      this.timer = setTimeout(this.execute.bind(this));
+    });
+  },
+};
+
 export const onRequest = (ctx: EOEventContext) => {
   if (!hono) {
     hono = createHono({
@@ -25,6 +112,9 @@ export const onRequest = (ctx: EOEventContext) => {
       urlPrefix: ctx.env.URL_PREFIX || '/',
       basicAuth: ctx.env.BASIC_AUTH,
       apnsUrl: ctx.env.APNS_URL,
+      requestAPNs: ctx.env.APNS_URL
+        ? undefined
+        : APNsProxy.request.bind(APNsProxy),
     });
   }
   return hono.fetch(ctx.request, ctx.env);
